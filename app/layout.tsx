d0d61fef5e8149f3f0aa90f1d563e5cd07d9f6a5
KODAK <ivynjoroge30@gmail.com>
2024-09-29 22:07:11 +0300

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Ivys's Portfolio",
+  description: "Personal Portfolio",
 };
 
 export default function RootLayout({
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgNTnUD3uTW6hXsWSeXN9gYy7z351-ut5Xwg&s" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
